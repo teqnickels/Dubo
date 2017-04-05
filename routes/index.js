d7b578624518051cbbda524d3641ab8b89e26d91
Debrena McEwen <debrenamcewen@gmail.com>
2017-04-05 12:04:50 -0700

    add each loop

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,25 @@ var router = express.Router();
 var allTasks = require('../db/db.js')
 
   /* GET home page. */
-  router.get('/', function(request, response, next) {
-    res.render('index', { title: 'Express'});
-  });
-
-  router.post('/create', (req, res) => {
-    allTasks.create(req.body.task).then( () =>
-      res.redirect('/')
-    )
+  router.get('/', function( request, response ) {
+    allTasks.showAll().then(task => {
+      response.render('index', {
+        title: 'To Do List',
+        allTasks: allTasks
+      })
+    })
     .catch(error => res.json(error))
   })
 
+  router.post('/create', (request, response) => {
+    allTasks.create(request.body.task).then( () =>
+      response.redirect('/')
+    )
+    .catch(error => response.json(error))
+  })
 
+//write a function that loops through the object alltasks
+//and returns the value here
+//pass that value into the object at literal in render
 
 module.exports = router;
